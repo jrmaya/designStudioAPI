7e1c9e56ac5b2dbafe58112957028e9e49cce3b6
jrmaya <jromanmz@gmail.com>
2017-06-09 20:29:05 +1000

    mex

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.use('/email', emailManager);
 //Start the server
 //===========================================================================================================
 app.listen(port);
-console.log('Server running on  ' + port);
+console.log('ESTAMOS EN EL PUERTO NO.  ' + port);
 
 //export DB to be available to the controllers
 module.exports = mongoose;
