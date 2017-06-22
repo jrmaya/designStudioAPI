8531ff814dbc4b300e3dc584f9ab02f828303f1e
Roman Maya <jromanmz@gmail.com>
2017-06-22 19:04:51 +1000

    FIXED CORS

--- cors.js
+++ cors.js
@@ -1,8 +1,8 @@
 module.exports = function() {
   return function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Access-Control-Allow-Headers', 'x-access-token, Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'x-access-token, Content-Type, Authorization, Content-Length, X-Requested-With,content-type');
     next();
   };
 }
